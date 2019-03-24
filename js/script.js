e9c8bb32d90fde4c9d1d06771321943ae09016aa
Kate Speed <katespeed@gmail.com>
2019-03-24 19:24:43 -0400

    Smooth scrolling to top and clean up js file

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 
-// functions are in here so that the DOM loads before they are called
+/** 
+ * functions are in here so that the DOM loads before they are called
+ */
 
 $(function() {
 
-  const navIcon = $('#nav-icon');
-
-  /* clicking on hamburger icon opens the navigation sidebar
-  */
-  navIcon.on('click', function() {
+  /** 
+   * clicking on hamburger icon opens the navigation sidebar
+   */
+  $('#nav-icon').on('click', function() {
     $('body').toggleClass('nav-hidden');
   });
 
@@ -15,19 +16,9 @@ $(function() {
    * clicking on arrow icon takes user back to top of page
    */
   $('.back-to-top').on('click', 'i', function () {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html, body').animate({ scrollTop: 0 }, "slow"); return false;
   });
-
-  /* placeholder code for smooth jump 
-  $('.scroll_to').click(function(e){
-    var jump = $(this).attr('href');
-    var new_position = $(jump).offset();
-    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
-    e.preventDefault();
-  });
-  */
-
+ 
 }());
   
 

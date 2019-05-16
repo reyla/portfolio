42d102158b10ba68962f067e24c0b86c8272faa9
Kate Speed <katespeed@gmail.com>
2019-05-15 21:05:40 -0400

    Add sliding effect to elements with jquery

--- js/script.js
+++ js/script.js
@@ -18,7 +18,82 @@ $(function() {
   $('.back-to-top').on('click', 'i', function () {
     $('html, body').animate({ scrollTop: 0 }, "slow"); return false;
   });
- 
-}());
+
+
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+  
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  
+  
+  var win = $(window);
+  
+  /* find all the elements that want to slide around */
+  var sideMods = $('.module-side');
+  var upMods = $('.module-upwards');
+  
+  /* prevent things from sliding multiple times */
+  sideMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass('already-visible'); 
+    } 
+  });
+
+  upMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass('already-visible'); 
+    } 
+  });
+  
+
+  /* when the element is visible in the viewport, make it slide! */
+  win.scroll(function(event) {
+    
+    sideMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass('come-in-sideways'); 
+      } 
+    });
+
+    upMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass('come-in-upwards'); 
+      } 
+    });
+    
+  });
+
+})(jQuery);
+
+
+
+
   
 
